1e7a81f3634170165b70db77d3c884ebd3a1b31b
Your Name <you@example.com>
2026-09-17 08:44:58 +0545

    gg

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const app = express()
+//specify the format will be json
+app.use(express.json())
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+  res.send('Hello World!')
+})
+
+//making our first request
+app.get("/name",(req,res)=>{
+  //header value
+  console.log("header value:",req.headers.myheader)
+  //getting params
+  console.log("params value:",req.query.myparams)
+  //response
+  res.status(201).json({
+    "message":"Shrayo Maharjan"
+  })
+})
+
+//endpoint post to get body value
+app.post("/data",(req,res)=>{
+  console.log(req.body)
+
+  res.status(200).json({
+    "message":"successfully posted",
+  })
+})
 
 app.listen(port, () => {
-  console.log(`example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
